2b50e8b4b6834c7a5d9e414bbd712fe5234ab776
Ernesto Serrano <info@ernesto.es>
2019-05-27 17:17:21 +0200

    Adde hello world in python

---
+++ homework_01_python/exercise_01.py
@@ -0,0 +1,13 @@
+
+
+def hello_world():
+    return 'Hola mundo!'
+
+
+# Prueba la función con los valores (4, 5)
+# Resultado: 9 (True)
+def test_hello_world():
+    assert hello_world() == 'Hola mundo!'
+
+
+print(hello_world())
---
+++ homework_01_python/exercise_02.py
@@ -0,0 +1,10 @@
+
+
+def add(x, y):
+    return x + y
+
+
+# Prueba la función con los valores (4, 5)
+# Resultado: 9 (True)
+def test_add():
+    assert add(4, 5) == 9
